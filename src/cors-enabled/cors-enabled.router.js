8f5108da523ab84a5129acc01a5c4840708556ac
Brett <brett_neighbors@yahoo.com>
2024-03-18 21:47:07 -0600

    Removed router.use(cors()) from cors-enabled.router

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -3,14 +3,11 @@ const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const cors = require("cors")
 
-router.use(cors())
-
 router
   .route("/:corsId")
   .get(controller.read)
   .put(controller.update)
   .delete(controller.delete)
-  .options(corsDelete)
   .all(methodNotAllowed);
 
 router
